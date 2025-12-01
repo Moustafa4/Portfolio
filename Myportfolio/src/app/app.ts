b1fda01fc00f67a88b196535f40d906debc5fa5f
Moustafa Mahmoud <moustafamahmoud5600@gmail.com>
2025-12-01 09:52:17 +0200

    .

--- Myportfolio/src/app/app.ts
+++ Myportfolio/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Home } from './componnent/home/home';
 import { Header } from './componnent/header/header';
 import { RouterModule } from "@angular/router";
+import { ServicesPart } from "./componnent/services-part/services-part";
 
 @Component({
   selector: 'app-root',
-  imports: [Home, Header, RouterModule],
+  imports: [Home, Header, RouterModule, ServicesPart],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ Myportfolio/src/app/componnent/services-part/services-part.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-part',
+  imports: [],
+  templateUrl: './services-part.html',
+  styleUrl: './services-part.css',
+})
+export class ServicesPart {
+
+}
