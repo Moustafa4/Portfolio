2caf02602ccf1be4f4dc8f350adaaf038843d934
Moustafa Mahmoud <moustafamahmoud5600@gmail.com>
2025-12-09 11:04:04 +0200

    .

--- Myportfolio/src/app/componnent/home_parts/about-me/about-me.ts
+++ Myportfolio/src/app/componnent/home_parts/about-me/about-me.ts
@@ -7,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.css',
 })
 export class AboutMe {
+  ngAfterViewInit() {
+    // const svg = document.getElementsByTagName('svg');
+    const divcon = document.querySelectorAll('.skills_con .svg_con');
+    let section = document.querySelector('.skills_row') as HTMLElement;
+    let startedanimation = false;
 
+window.addEventListener('scroll', () => {
+  const rect = section.getBoundingClientRect();
+  if (!startedanimation && rect.top <= window.innerHeight - 100) {
+    startedanimation = true;
+    divcon.forEach((div) => started(div));
+    console.log("Animation started");
+  }
+});
+
+
+    function started(el: any) {
+      let goal = +el.dataset.goal;
+      let circle = el.getElementsByTagName('circle')[0];
+      let rad = circle.r.baseVal.value;
+      let par = Number(2 * Math.PI * rad);
+      circle.style.strokeDasharray = `${par}`;
+      circle.style.strokeDashoffset = `${par}`;
+      let currentval = par;
+      let targetval = par * (1 - goal / 100);
+      let step = (par - targetval) / 60;
+      // console.log(currentval);
+      // console.log(targetval);
+      const anim = setInterval(() => {
+        currentval -= step;
+
+        if (currentval <= targetval) {
+          circle.style.strokeDashoffset = `${targetval}`;
+          clearInterval(anim);
+        }
+        circle.style.strokeDashoffset = `${currentval}`;
+      }, 3000 / goal);
+    }
+  }
 }
