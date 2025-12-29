4555c7ea1367226cb0c5d9fac8fcc5e77b9b9346
Moustafa Mahmoud <moustafamahmoud5600@gmail.com>
2025-12-29 20:09:41 +0200

    .

--- Myportfolio/src/app/componnent/home/home.ts
+++ Myportfolio/src/app/componnent/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AboutMe } from "../home_parts/about-me/about-me";
 import { ServicesPart } from "../home_parts/services-part/services-part";
 import { ProjHome } from "../home_parts/proj-home/proj-home";
+import { Contact } from "../contact/contact";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutMe, ServicesPart, ProjHome],
+  imports: [AboutMe, ServicesPart, ProjHome, Contact],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
