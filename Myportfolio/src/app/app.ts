4905560a9df679358e9edc3cdd9658acf67f3dcf
Moustafa Mahmoud <moustafamahmoud5600@gmail.com>
2026-01-01 14:58:01 +0200

    .

--- Myportfolio/src/app/app.ts
+++ Myportfolio/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Header } from './componnent/header/header';
 import { RouterModule } from "@angular/router";
 import { Footer } from "./componnent/footer/footer";
+import { Contact } from "./componnent/contact/contact";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, RouterModule, Footer],
+  imports: [Header, RouterModule, Footer, Contact],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
